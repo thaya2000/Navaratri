0d495b1a58a976933cd209ba81359a425e8a790a
Thayanan Thevarasa <thevarasathayanan@outlook.com>
2023-11-24 21:04:09 +0530

    update_3.0

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import "./App.css"; // Import your styles
 import Header from "./components/Header";
 import Countdown from "./components/Countdown";
 import HomeAnimation from "./components/HomeAnimation";
+import Text from "./components/Text";
 
 function App() {
   return (
     <>
-      <HomeAnimation/>
-      <Countdown/>
+      <HomeAnimation />
+      <Text />
+      <Countdown />
     </>
   );
 }
---
+++ src/components/Text.jsx
@@ -0,0 +1,20 @@
+import "./Text.css";
+
+
+export default function Text() {
+  return (
+    <>
+      <div className="text">
+        <p> We extend a gracious invitation to.</p>
+        <h1>DR. KUSHAN SUDHEERA</h1>
+        {/* <h2>etuhj;jpup tpoh 2023</h2> */}
+        <img src="https://raw.githubusercontent.com/thaya2000/image_upload/main/Navaratri.png" />
+        <h2>Navaratri Vizha 2023</h2>
+        <h3>DATE : 20.11.2023</h3>
+        <h3>VENUE : AUDITORIUM</h3>
+        <h3>TIME : 5.05PM ONWARDS</h3>
+        <h4>Organized by EFSU</h4>
+      </div>
+    </>
+  );
+}

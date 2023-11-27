c0c081d1134ca38dc142a1c9693649945af0620a
Thayanan Thevarasa <thevarasathayanan@outlook.com>
2023-11-27 10:51:11 +0530

    update_11.0

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
 import React from "react";
+import { BrowserRouter, HashRouter, Routes, Route } from "react-router-dom";
 import "./App.css"; // Import your styles
 import Home from "./pages/Home";
 
 function App() {
   return (
     <>
-      <Home />
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path="/Navaratri/:index" element={<Home />} /> */}
+          <Route path="/Navaratri/" element={<Home />} />
+          <Route path="/Navaratri/0" element={<Home />} />
+          <Route path="/Navaratri/:index" element={<Home />} />
+          {/* <Route path="/Navaratri/" element={<Home />} /> */}
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/components/Text.jsx
+++ src/components/Text.jsx
@@ -2,14 +2,22 @@ import "./Text.css";
 import Navaratri from "../assets/img/Navaratri.png";
 import Navaratri_text from "../assets/img/Navatri_text.png";
 import Navaratri_animation from "../assets/img/Navaratri_text_animation.png";
-
+import { useParams } from "react-router-dom";
 
 export default function Text() {
+  const { index } = useParams();
+
+  var inviteesList = {
+    1: "John Doe",
+    2: "Jane Smith",
+    3: "DR. KUSHAN SUDHEERA",
+    guest3: "Bob Johnson",
+  };
   return (
     <>
       <div className="text">
         <p> We extend a gracious invitation to.</p>
-        <h1>DR. KUSHAN SUDHEERA</h1>
+        <h1>{index == null ? "KUSHAN SUDHEERA" : inviteesList[index]}</h1>
         {/* <h2>etuhj;jpup tpoh 2023</h2> */}
         <img src={Navaratri_text} alt="" />
         <h2>Navaratri Vizha 2023</h2>
